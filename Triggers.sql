3906df215148c27054732b71447c296cb9a149c6
fiito720 <89756491+fiito720@users.noreply.github.com>
2022-12-29 12:04:06 -0600

    Create Triggers.sql

---
+++ Triggers.sql
@@ -0,0 +1,15 @@
+------- GenderCreation
+CREATE OR REPLACE TRIGGER proj.beforeInsertGender
+BEFORE INSERT ON proj.Gender
+FOR EACH ROW 
+BEGIN
+:new.creationUser:=USER; :new.creationDate:=SYSDATE; 
+END beforeInsertGender;
+
+------- GenderModification
+CREATE OR REPLACE TRIGGER proj.beforeUpdateGender
+BEFORE UPDATE ON proj.Gender 
+FOR EACH ROW 
+BEGIN 
+:new.modificationUser:=USER; :new.modificationDate:=SYSDATE; 
+END beforeUpdateGender;
