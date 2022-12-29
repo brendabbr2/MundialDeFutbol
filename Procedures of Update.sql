b08ffad51fcd499feaa1f032d7edd1b649d55c45
fiito720 <89756491+fiito720@users.noreply.github.com>
2022-12-29 14:49:05 -0600

    Update Procedures of Update.sql

--- Procedures of Update.sql
+++ Procedures of Update.sql
@@ -75,6 +75,68 @@ CREATE OR REPLACE PROCEDURE updateStadiumXSportMatch
     --COMMIT; 
 END updateStadiumXSportMatch;
 
+-------------------------------------------------------------------------------------------------
+-- By: Rodolfo Acuña Lopez
 
+-- Update Gender
+CREATE OR REPLACE PROCEDURE updateGender 
+    (V_idGender IN NUMBER, V_descriptionGender IN VARCHAR2)
+    AS BEGIN
+        UPDATE Gender
+        SET descriptionGender = NVL(V_descriptionGender, descriptionGender)
+        WHERE idGender = V_idGender;
+    --COMMIT; 
+END updateGender;
 
+-- Update Worker
+CREATE OR REPLACE PROCEDURE updateWorker 
+    (V_idWorker IN NUMBER, V_hireDate IN DATE)
+    AS BEGIN
+        UPDATE Worker
+        SET hireDate = NVL(V_hireDate, hireDate)
+        WHERE idWorker = V_idWorker;
+    --COMMIT; 
+END updateWorker;
 
+-- Update WorkerType
+CREATE OR REPLACE PROCEDURE updateWorkerType 
+    (V_idWorkerType IN NUMBER, V_nameWorkerType IN VARCHAR2)
+    AS BEGIN
+        UPDATE WorkerType
+        SET nameWorkerType = NVL(V_nameWorkerType, nameWorkerType)
+        WHERE idWorkerType = V_idWorkerType;
+    --COMMIT; 
+END updateWorkerType;
+
+-- Update PlayerPosition
+CREATE OR REPLACE PROCEDURE updatePlayerPosition 
+    (V_idPosition IN NUMBER, V_descriptionPos IN VARCHAR2)
+    AS BEGIN
+        UPDATE PlayerPosition
+        SET descriptionPos = NVL(V_descriptionPos, descriptionPos)
+        WHERE idPosition = V_idPosition;
+    --COMMIT; 
+END updatePlayerPosition;
+
+-- Update PlayerXSportMatch
+CREATE OR REPLACE PROCEDURE updatePlayerXSportMatch 
+    (v_idPlayer IN NUMBER, v_idSportMatch IN NUMBER, 
+    v_yellow_card IN INTEGER, v_red_card IN INTEGER, 
+    v_savers IN INTEGER, v_offside IN INTEGER,
+    v_expulsion IN INTEGER, v_goals IN INTEGER, 
+    v_corners IN INTEGER)
+    AS BEGIN
+        UPDATE PlayerXSportMatch
+        SET yellow_card = NVL(V_yellow_card, yellow_card),
+            red_card = NVL(V_red_card, red_card),
+            savers = NVL(V_savers, savers),
+            offside = NVL(V_offside, offside),
+            expulsion = NVL(V_expulsion, expulsion),
+            goals = NVL(V_goals, goals),
+            corners = NVL(V_corners, corners)
+        WHERE idPlayer = v_idPlayer 
+        AND idSportMatch = v_idsportmatch;
+    --COMMIT;
+END updatePlayerXSportMatch;
+
+--------------------------------------------------------------------------------------------
