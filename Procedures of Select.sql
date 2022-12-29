0a01ffbc4856a2d8f691b8c58d26c1389dd1cf9d
fiito720 <89756491+fiito720@users.noreply.github.com>
2022-12-28 20:20:03 -0600

    Update Procedures of Select.sql

--- Procedures of Select.sql
+++ Procedures of Select.sql
@@ -8,3 +8,50 @@ BEGIN
     WHERE idGender = NVL(v_idGender, idGender); 
     RETURN GenderCursor; 
 END getGender;
+
+
+--This is a procedure that return a cursor of the table Worker
+CREATE OR REPLACE FUNCTION getWorker(v_idWorker IN NUMBER) 
+RETURN SYS_REFCURSOR AS WorkerCursor SYS_REFCURSOR; 
+BEGIN 
+    OPEN WorkerCursor FOR  
+    SELECT idWorker, idWorkerType, hireDate 
+    FROM Worker  
+    WHERE idWorker = NVL(v_idWorker, idWorker); 
+    RETURN WorkerCursor; 
+END getWorker;
+
+--This is a procedure that return a cursor of the table WorkerType
+CREATE OR REPLACE FUNCTION getWorkerType(v_idWorkerType IN NUMBER) 
+RETURN SYS_REFCURSOR AS WorkerTypeCursor SYS_REFCURSOR; 
+BEGIN 
+    OPEN WorkerTypeCursor FOR  
+    SELECT idWorkerType, nameWorkerType 
+    FROM WorkerType  
+    WHERE idWorkerType = NVL(v_idWorkerType, idWorkerType); 
+    RETURN WorkerTypeCursor; 
+END getWorkerType;
+
+--This is a procedure that return a cursor of the table PlayerPosition
+CREATE OR REPLACE FUNCTION getPlayerPosition(v_idPlayerPosition IN NUMBER) 
+RETURN SYS_REFCURSOR AS PlayerPositionCursor SYS_REFCURSOR; 
+BEGIN 
+    OPEN PlayerPositionCursor FOR  
+    SELECT idPosition, positionName 
+    FROM PlayerPosition  
+    WHERE idPosition = NVL(v_idPlayerPosition, idPosition); 
+    RETURN PlayerPositionCursor; 
+END getPlayerPosition;
+
+--This is a procedure that return a cursor of the table PlayerXSportMatch
+CREATE OR REPLACE FUNCTION getPlayerXSportMatch(v_idPlayer IN NUMBER, 
+    v_idSportMatch IN NUMBER) 
+RETURN SYS_REFCURSOR AS PlayerXSportMatchCursor SYS_REFCURSOR; 
+BEGIN 
+    OPEN PlayerXSportMatchCursor FOR  
+    SELECT idPlayer, idSportMatch, yellow_card, red_card, savers, offside, expulsion, goals, corners 
+    FROM PlayerXSportMatch  
+    WHERE idPlayer = NVL(v_idPlayer, idPlayer)
+    AND idSportMatch = NVL(v_idSportMatch, idSportMatch); 
+    RETURN PlayerXSportMatchCursor; 
+END getPlayerXSportMatch;
