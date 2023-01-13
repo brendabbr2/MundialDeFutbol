1c67efce9c65e12e868135b316e897e88f0d7fb9
fiito720 <89756491+fiito720@users.noreply.github.com>
2023-01-13 12:58:28 -0600

    New sql for the comments of the tables

---
+++ Scripts SQL/Comments for tables.sql
@@ -0,0 +1,20 @@
+COMMENT ON TABLE Gender
+IS 'This table is for save the genders of a person that going to be necessary in data base for a personal information';
+
+COMMENT ON TABLE Phone
+IS 'This table is for save the number phones of people that going to be necessary in data base for a personal information';
+
+COMMENT ON TABLE Email
+IS 'This table is for save the emails of people that going to be necessary in data base for a personal information';
+
+COMMENT ON TABLE Identification
+IS 'This table is for save the indentification of a person that going to be necessary in data base for a personal information';
+
+COMMENT ON TABLE IdentificationType
+IS '';
+
+COMMENT ON TABLE Person
+IS '';
+
+COMMENT ON TABLE Usertype
+IS '';
