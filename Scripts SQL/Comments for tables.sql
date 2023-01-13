7206425132aa8c9ff5b2dcfe78d4cccd08af1868
fiito720 <89756491+fiito720@users.noreply.github.com>
2023-01-13 17:14:59 -0600

    New comments for tables

--- Scripts SQL/Comments for tables.sql
+++ Scripts SQL/Comments for tables.sql
@@ -1,20 +1,20 @@
 COMMENT ON TABLE Gender
-IS 'This table is for save the genders of a person that going to be necessary in data base for a personal information';
+IS 'This table is for save the genders of a person that going to be necessary in data base for a personal information.';
 
 COMMENT ON TABLE Phone
-IS 'This table is for save the number phones of people that going to be necessary in data base for a personal information';
+IS 'This table is for save the number phones of people that going to be necessary in data base for a personal information.';
 
 COMMENT ON TABLE Email
-IS 'This table is for save the emails of people that going to be necessary in data base for a personal information';
+IS 'This table is for save the emails of people that going to be necessary in data base for a personal information.';
 
 COMMENT ON TABLE Identification
-IS 'This table is for save the indentification of a person that going to be necessary in data base for a personal information';
+IS 'This table is for save the indentification of a person that going to be necessary in data base for a personal information.';
 
 COMMENT ON TABLE IdentificationType
-IS '';
+IS 'This table is for save type of personal identification that going to be necessary in data base. the type of identification is specified in the mask attribute.';
 
 COMMENT ON TABLE Person
-IS '';
+IS 'In the conceptual model, this is going to be like a super class because here is going to be register';
 
 COMMENT ON TABLE Usertype
-IS '';
+IS 'This table is for save a type of user in a resgister';
